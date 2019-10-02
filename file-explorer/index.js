e921da1831f59fa677c2fae3e9d9c1081670d3d4
Qbin <bingling.qb@raycloud.com>
2019-10-02 16:59:09 +0800

    支持读取文件或目录

--- file-explorer/index.js
+++ file-explorer/index.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var stdin = process.stdin;
 var stdout = process.stdout;
+var stats = [];
 // 打印当前同级目录下的其他文件 同步版本
 // console.log(fs.readdirSync(__dirname));
 
@@ -24,9 +25,12 @@ fs.readdir(process.cwd(), function(err, files){
     var filename = files[i];
     // fs.stat 会给出文件或目录的元数据
     fs.stat(__dirname + '/' + filename, function(err, stat){
+      stats[i] = stat;
       if(stat.isDirectory()){
+        // 如果是文件夹
         console.log(' ' + i + '     \033[36m' + filename + '/\033[39m');
       } else {
+        // 如果是文件
         console.log(' ' + i + '     \033[90m' + filename + '\033[39m');
       }
   
@@ -46,6 +50,38 @@ fs.readdir(process.cwd(), function(err, files){
     stdin.resume();
     // 设置流编码为utf8，未来支持特殊字符
     stdin.setEncoding('utf8');
+    // 监听data事件
+    stdin.on('data', option);
+  }
+
+  function option(data){
+    var filename = files[Number(data)];
+
+    // if(!filename){
+    //   stdout.write('   \033[31mEnter your choice: \033[39m');
+    // }else{
+    //   stdin.pause();
+    //   fs.readFile(__dirname + '/' + filename, 'utf8', function(err, data){
+    //     console.log('');
+    //     console.log('\033[90m' + data.replace(/(.*)/g, '    $1') +'\033[39m');
+    //   })
+    // }
+
+    if(stats[Number(data)].isDirectory()){
+      fs.readdir(__dirname + '/' + filename, function(err, files){
+        console.log('');
+        console.log('  (' + files.length + ' files)')
+          files.forEach(function(file){
+            console.log('     -   ' + file);
+          });
+          console.log()
+      })
+    }else{
+      fs.readFile(__dirname + '/' + filename, 'utf8', function(err, data){
+        console.log('');
+        console.log('\033[90m' + data.replace(/(.*)/g, '    $1') +'\033[39m');
+      })
+    }
   }
 
   file(0);
